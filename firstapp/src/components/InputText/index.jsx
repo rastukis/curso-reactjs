6a5c1ec1df1bedceca4c218f5ab29811d3ebaf76
Migue Ángel Plascencia Cruz <mplascencia.cruz@gmail.com>
2019-01-28 21:26:12 -0600

    Campo para capturar datos.

--- firstapp/src/components/InputText/index.jsx
+++ firstapp/src/components/InputText/index.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
+import styles from './InputText.css';
+
 class InputText extends Component{
 
     render(){
-        return <div>InputText</div>
+        return (
+            <form className={styles.form} onSubmit={this.props.onSendText}>
+                <textarea className={styles.text} name='text'> </textarea>
+                <div className={styles.buttons}>
+                    <button className={styles.close} onClick={this.props.onCloseText}>Cerrar</button>
+                    <button className={styles.send} type='submit'>Enviar</button>
+                </div>
+            </form>
+        )
     }
 }
 
